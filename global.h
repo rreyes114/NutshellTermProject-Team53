0786dd6f0ab6105f5303c0253d4dd89c03e3831d
rreyes114 <rreyes1@ufl.edu>
2021-04-12 20:12:10 -0400

    added global header

---
+++ global.h
@@ -0,0 +1,22 @@
+#include "stdbool.h"
+#include <limits.h>
+
+typedef struct evTable {
+   char var[128][100];
+   char word[128][100];
+} evTable;
+
+typedef struct aTable {
+	char name[128][100];
+	char word[128][100];
+} aTable;
+
+evTable varTable;
+
+aTable aliasTable;
+
+char cwd[PATH_MAX];
+
+int aliasIndex, varIndex;
+
+char* subAliases(char* name);
--- nutshparser.y
+++ nutshparser.y
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include "global.h"
 
 int yylex(void);
 int yyerror(char *s);
