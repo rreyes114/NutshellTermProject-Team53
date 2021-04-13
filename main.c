7f5fec881daf7c287a83dcce60a11e7717fba3eb
Josh Ryals <joshryals3@gmail.com>
2021-04-12 22:14:52 -0400

    add main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "global.h"
+#include <unistd.h>
+
+char *getcwd(char *buf, size_t size);
+int yyparse();
+
+int main()
+{
+    aliasIndex = 0;
+    varIndex = 0;
+
+    getcwd(cwd, sizeof(cwd));
+
+    strcpy(varTable.var[varIndex], "PWD");
+    strcpy(varTable.word[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "HOME");
+    strcpy(varTable.word[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PROMPT");
+    strcpy(varTable.word[varIndex], "nutshell");
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PATH");
+    strcpy(varTable.word[varIndex], ".:/bin");
+    varIndex++;
+
+    system("clear");
+    while(1)
+    {
+        printf("[%s]>> ", varTable.word[2]);
+        yyparse();
+    }
+
+   return 0;
+}
