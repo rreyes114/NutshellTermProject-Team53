5a7eb3b87ddee4ec5bb766a188713a579dfb415a
rreyes114 <rreyes1@ufl.edu>
2021-04-13 20:49:18 -0400

    created command table struct and global index.

--- global.h
+++ global.h
@@ -11,12 +11,36 @@ typedef struct aTable {
 	char word[128][100];
 } aTable;
 
+typedef struct cTable {
+	char name[128][100];
+	int argcnt[128];
+	char args[128][128][100];
+	char infile[128][100];
+	char outfile[128][100];
+} cTable;
+
+/*
+typedef struct bcommand {
+	char name[16];
+	int argcnt;
+	char args[128][100];
+	char infile[16];
+	char outfile[16];
+} bcommand;
+
+typedef struct cTable {
+	bcommand bcommands[128];
+} cTable;
+*/
+
 evTable varTable;
 
 aTable aliasTable;
 
-char cwd[PATH_MAX];
+cTable cmdTable;
 
-int aliasIndex, varIndex;
+int aliasIndex, varIndex, cmdIndex;
+
+char cwd[PATH_MAX];
 
 char* subAliases(char* name);
---
+++ nutshell.c
@@ -0,0 +1,42 @@
+// This is ONLY a demo micro-shell whose purpose is to illustrate the need for and how to handle nested alias substitutions and how to use Flex start conditions.
+// This is to help students learn these specific capabilities, the code is by far not a complete nutshell by any means.
+// Only "alias name word", "cd word", and "bye" run.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "global.h"
+#include <unistd.h>
+
+char *getcwd(char *buf, size_t size);
+int yyparse();
+
+int main()
+{
+    aliasIndex = 0;
+    varIndex = 0;
+	cmdIndex = 0;
+
+    getcwd(cwd, sizeof(cwd));
+
+    strcpy(varTable.var[varIndex], "PWD");
+    strcpy(varTable.word[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "HOME");
+    strcpy(varTable.word[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PROMPT");
+    strcpy(varTable.word[varIndex], "nutshell");
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PATH");
+    strcpy(varTable.word[varIndex], ".:/bin");
+    varIndex++;
+
+    system("clear");
+    while(1)
+    {
+        printf("[%s]>> ", varTable.word[2]);
+        yyparse();
+    }
+
+   return 0;
+}
